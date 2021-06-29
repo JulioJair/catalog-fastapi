3925c64783bce6e4b4968f678bec8433b9e81aab
Julio de Alba <juliojair@outlook.com>
2021-06-28 22:44:42 -0500

    fix: update schema analytic

--- backend/app/app/schemas/analytic.py
+++ backend/app/app/schemas/analytic.py
@@ -1,19 +1,23 @@
 from pydantic import BaseModel, Field
-from typing import Optional
-from fastapi import Path
-import datetime
 
 
 class AnalyticBase(BaseModel):
-    product_id: str = Field(description="Product id")
     times_requested: str = Field(
         description="Number of times the product has been queried by users")
+    product_id: str = Field(description="Product id")
 
+
+class ProductOut(BaseModel):
+    from .user import UserOut
+    sku: str = Field(description="Product identifier")
+    name: str
+    price: float
+    brand: str
+    editor: UserOut = None
+    class Config():
+        orm_mode = True
 
 class Analytic(AnalyticBase):
-    product_id: str = Field(description="Product id")
-    times_requested: str = Field(
-        description="Number of times the product has been queried by users")
-
+    product: ProductOut = None
     class Config():
         orm_mode = True
